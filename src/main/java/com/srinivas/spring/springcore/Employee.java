affa1c89a347b0adee22ab429a31a306c01c732e
Sindhu Bharthepudi <DJ285ZC@US.NA.EY.NET>
2017-12-03 13:50:05 -0500

    changed id to number

--- src/main/java/com/srinivas/spring/springcore/Employee.java
+++ src/main/java/com/srinivas/spring/springcore/Employee.java
@@ -2,19 +2,19 @@ package com.srinivas.spring.springcore;
 
 public class Employee {
 
-	private int id;
+	private int number;
 	private String name;
 	
-	public int getId() {
-		return id;
+	public int getNumber() {
+		return number;
 	}
-	public void setId(int id) {
-		this.id = id;
+	public vonumber setNumber(int number) {
+		this.number = number;
 	}
 	public String getName() {
 		return name;
 	}
-	public void setName(String name) {
+	public vonumber setName(String name) {
 		this.name = name;
 	}
 
